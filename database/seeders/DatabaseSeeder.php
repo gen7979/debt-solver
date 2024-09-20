04a62fea9302d614b1195537003f73562358f69f
服部元志 <hatowork.7979@gmail.com>
2024-09-21 00:45:27 +0900

    fix: factoryの実装

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        $this->call([
-            initUsersSeeder::class,
-        ]);
+        // $this->call([
+        //     initUsersSeeder::class,
+        // ]);
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
