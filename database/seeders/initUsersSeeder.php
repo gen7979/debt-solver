e457cc70ab6945703fb5b01a62196eb3b905d9f0
gen7979 <144446689+gen7979@users.noreply.github.com>
2024-09-21 00:46:31 +0900

    Des 001 (#1)
    * fix: seedの実装
    
    * fix: factoryの実装

---
+++ database/seeders/initUsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class initUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Str::random(10),
+        ]);
+    }
+}
