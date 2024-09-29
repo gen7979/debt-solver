3423860528dc92754b8f6afbde7dbcb9b2d7f37b
服部元志 <hatowork.7979@gmail.com>
2024-09-29 14:55:13 +0900

    fix: laravel breezeの導入

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,21 @@
 <?php
 
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\DebtCalculateController;
-use App\Http\Controllers\DebtRegisterController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/register', [DebtRegisterController::class, 'index']);
 Route::post('/calculate', [DebtCalculateController::class, 'calculate']);
 
@@ -14,3 +23,5 @@ Route::post('/calculate', [DebtCalculateController::class, 'calculate']);
 Route::get('/phpinfo', function () {
     phpinfo();
 });
+
+require __DIR__.'/auth.php';
