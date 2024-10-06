9046e42db75c5e4346654ce10fa4543a7dcc294e
服部元志 <hatowork.7979@gmail.com>
2024-10-06 14:34:06 +0900

    fix: マイグレーションファイルを作成

---
+++ database/migrations/2024_10_06_052232_create_debts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('debts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('company_name');
+            $table->integer('remaining_amount');
+            $table->integer('interest_rate');
+            $table->integer('repayment_amount');
+            $table->integer('repayment_day');
+            $table->boolean('is_repaid')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('debts');
+    }
+};
