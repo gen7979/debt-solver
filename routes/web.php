f5f80aecb9897840c846ec1b0bff9f16fb765426
服部元志 <hatowork.7979@gmail.com>
2024-10-06 13:41:41 +0900

    ifx: ログイン後、ユーザー登録後にdashboardに遷移しないように修正

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,7 @@ Route::get('/', function () {
     return view('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/debt-register', [DebtRegisterController::class, 'index'])->name('debt-register');
     Route::post('/calculate', [DebtCalculateController::class, 'calculate'])->name('calculate');
 });
