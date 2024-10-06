a25ee2c35dbb9f762cc61bfd7a2ede7efbec3b74
服部元志 <hatowork.7979@gmail.com>
2024-10-06 13:58:08 +0900

    fix: ひとまずはレイアウトをbootstrap に変更

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,33 +1,34 @@
 <html>
   <head>
-    <title>Login</title> <!-- タイトルを変更 -->
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   </head>
   <body>
     <h1>Login Page</h1>
     @if (Route::has('login'))
       <nav class="-mx-3 flex flex-1 justify-end">
         @auth
-          <a
-            href="{{ url('/dashboard') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-          >
-            Dashboard
-          </a>
+          <form action="{{ route('debt-register') }}">
+            @csrf
+            <button type="submit" class="btn btn-outline-primary">
+              借金計算
+            </button>
+          </form>
         @else
-          <a
-            href="{{ route('login') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-          >
-            Log in
-          </a>
-
+          <form action="{{ route('login') }}">
+            @csrf
+            <button type="submit" class="btn btn-outline-primary">
+              ログイン
+            </button>
+          </form>
           @if (Route::has('register'))
-            <a
-              href="{{ route('register') }}"
-              class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-            >
-              Register
-            </a>
+            <form action="{{ route('register') }}">
+              @csrf
+              <button type="submit" class="btn btn-outline-primary">
+                新規登録
+              </button>
+            </form>
           @endif
         @endauth
       </nav>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,11 +9,10 @@
   <!-- ログアウトフォーム -->
   <form method="POST" action="{{ route('logout') }}">
     @csrf
-    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+    <button type="button" class="btn btn-outline-primary"
+            onclick="event.preventDefault(); this.closest('form').submit();">
       {{ __('Log Out') }}
-    </x-responsive-nav-link>
+    </button>
   </form>
 
   <!-- 入力フォーム -->
