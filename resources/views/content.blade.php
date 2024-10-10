bae2ef261432016f8949c4ae7cbec09c9f4cb63a
hat-7979 <g.hattori@stonesystem.co.jp>
2024-10-10 13:20:08 +0900

    fix: タブの切り替えに応じて表示を変更

---
+++ resources/views/content.blade.php
@@ -0,0 +1,78 @@
+<div class="mb-3 row">
+    <label class="col-sm-4 col-form-label fw-bold">{{ '会社名：' }}</label>
+    <div class="col-sm-8">
+        <span id="company-name">{{ $data['companyName'] }}</span>
+    </div>
+</div>
+<div class="mb-3 row">
+    <label class="col-sm-4 col-form-label fw-bold">{{ '残債：' }}</label>
+    <div class="col-sm-8">
+        <span id="loan-amount">{{ $data['loanAmount'] }}</span><span>円</span>
+    </div>
+</div>
+<div class="mb-3 row">
+    <label class="col-sm-4 col-form-label fw-bold">{{ '金利：' }}</label>
+    <div class="col-sm-8">
+        <span id="interest-rate">{{ $data['interestRates'] }}</span><span>%</span>
+    </div>
+</div>
+<div class="mb-3 row">
+    <label class="col-sm-4 col-form-label fw-bold">{{ '毎月の返済金額：' }}</label>
+    <div class="col-sm-8">
+        <span id="repayment-amount">{{ $data['repaymentAmount'] }}</span><span>円</span>
+    </div>
+</div>
+<div class="mb-3 row">
+    <label class="col-sm-4 col-form-label fw-bold">{{ '総額：' }}</label>
+    <div class="col-sm-8">
+        <span id="total-amount">{{ $data['totalAmount'] }}</span><span>円</span>
+    </div>
+</div>
+<div class="mb-3 row">
+    <label class="col-sm-4 col-form-label fw-bold">{{ '総利息：' }}</label>
+    <div class="col-sm-8">
+        <span id="total-interest">{{ $data['totalInterest'] }}</span><span>円</span>
+    </div>
+</div>
+<div class="mb-3 row">
+    <label class="col-sm-4 col-form-label fw-bold">{{ '返済期間：' }}</label>
+    <div class="col-sm-8">
+        <span id="repayment-periods">{{ $data['repaymentPeriods'] }}</span><span>ヶ月</span>
+    </div>
+</div>
+<div class="mb-3">
+    <span class="fw-bold">{{ '返済スケジュール：' }}</span>
+    <div class="accordion" id="accordionFlushExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                    返済スケジュールを表示
+                </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>期間</th>
+                                <th>返済金額</th>
+                                <th>利息</th>
+                                <th>残高</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($data['repaymentSchedule'] as $period => $details)
+                            <tr>
+                                <td>{{ $period }}</td>
+                                <td>{{ $details['repaymentAmount'] }}</td>
+                                <td>{{ $details['interestAmount'] }}</td>
+                                <td>{{ $details['remainingBalance'] }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
