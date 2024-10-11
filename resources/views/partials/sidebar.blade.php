e20ed6865854c52940cec572d69846fdca0db19d
hat-7979 <g.hattori@stonesystem.co.jp>
2024-10-11 10:03:13 +0900

    fix: 表示状態に応じてサイドバーのactiveを変更する修正

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,19 +5,19 @@
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-            <a href="/" class="nav-link active" aria-current="page">
+            <a href="/" class="nav-link {{Request::is('/') ? 'active' : ''}}" aria-current="page">
                 <i class="bi bi-house"></i>
                 マイページ
             </a>
         </li>
         <li class="nav-item">
-            <a href="/debt-register" class="nav-link" aria-current="page">
+            <a href="/debt-register" class="nav-link {{Request::is('debt-register') ? 'active' : ''}}" aria-current="page">
                 <i class="bi bi-house"></i>
                 登録フォーム
             </a>
         </li>
         <li class="nav-item">
-            <a href="/calculate" class="nav-link link-dark">
+            <a href="/calculate" class="nav-link {{Request::is('calculate') ? 'active' : ''}}">
                 <i class="bi bi-speedometer2"></i>
                 返済スケジュール
             </a>
